0c0ea8257f7946cd6cee1a2ff941d47b2d6cf9ec
Nieve2205 <166433352+Nieve2205@users.noreply.github.com>
2025-09-19 10:13:17 -0500

    Solucion de conflictos

---
+++ Caso5/Models/InventarioMateriasPrima.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Caso5.Models;
+
+public partial class InventarioMateriasPrima
+{
+    public int InventarioId { get; set; }
+
+    public int MateriaId { get; set; }
+
+    public decimal? Cantidad { get; set; }
+
+    public decimal? StockMinimo { get; set; }
+
+    public virtual MateriasPrima Materia { get; set; } = null!;
+}
