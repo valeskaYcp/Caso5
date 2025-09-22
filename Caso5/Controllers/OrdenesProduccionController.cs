a37e7e4971af30c03eeb9100c902a373b336bfd8
valeskaYcp <168563099+valeskaYcp@users.noreply.github.com>
2025-09-21 21:33:41 -0500

    endpoints OrdenesProduccionController

--- Caso5/Controllers/OrdenesProduccionController.cs
+++ Caso5/Controllers/OrdenesProduccionController.cs
@@ -25,5 +25,49 @@ namespace Caso5.Controllers
 
             return Ok(orden);
         }
+        
+        //orden de producción
+        [HttpPut("{id}/iniciar")]
+        public async Task<IActionResult> IniciarOrden(int id)
+        {
+            var orden = await _unitOfWork.Repository<OrdenesProduccion>().GetByIdAsync(id);
+            if (orden == null) return NotFound();
+
+            orden.FechaInicio = DateOnly.FromDateTime(DateTime.Today);
+            orden.Estado = "En Proceso";
+
+            _unitOfWork.Repository<OrdenesProduccion>().Update(orden);
+            await _unitOfWork.SaveAsync();
+
+            return Ok(orden);
+        }
+        
+        //Finalizar una orden
+        [HttpPut("{id}/finalizar")]
+        public async Task<IActionResult> FinalizarOrden(int id, [FromBody] decimal cantidadReal)
+        {
+            var orden = await _unitOfWork.Repository<OrdenesProduccion>().GetByIdAsync(id);
+            if (orden == null) return NotFound();
+
+            orden.FechaFin = DateOnly.FromDateTime(DateTime.Today);
+            orden.CantidadReal = cantidadReal;
+            orden.Estado = "Finalizada";
+
+            _unitOfWork.Repository<OrdenesProduccion>().Update(orden);
+            await _unitOfWork.SaveAsync();
+
+            return Ok(orden);
+        }
+        
+        //listar ordenes por estado
+        [HttpGet("por-estado/{estado}")]
+        public async Task<IActionResult> ObtenerPorEstado(string estado)
+        {
+            var ordenes = await _unitOfWork.Repository<OrdenesProduccion>()
+                .FindAsync(o => o.Estado == estado);
+            return Ok(ordenes);
+        }
+
+
     }
 }
